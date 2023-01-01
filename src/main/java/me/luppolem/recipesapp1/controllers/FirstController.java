7ee25ae0cabccee950d966b077cea72dd02f51b8
Lelena1 <luppol@inbox.ru>
2023-01-02 02:34:26 +0300

    Сделано ДЗ2 по web-разработке

--- src/main/java/me/luppolem/recipesapp1/controllers/FirstController.java
+++ src/main/java/me/luppolem/recipesapp1/controllers/FirstController.java
@@ -2,6 +2,7 @@ package me.luppolem.recipesapp1.controllers;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +12,9 @@ public class FirstController {
         return "Приложение запущено";
     }
 
+    @GetMapping("/info")
+    public String printInfo(@RequestParam String info){
+        return "Info: Проект #RecipesApp1 создан 1 января 2023 года Луппол Еленой и содержит сборник различных рецептов" + info;
+    }
 
 }
